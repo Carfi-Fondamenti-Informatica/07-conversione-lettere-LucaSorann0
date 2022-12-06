1ecc4d17b8f73593e5c085dcb860330f6b4bc3cc
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-12-06 21:29:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char let = 'a';
+  cin >> let;
+  if (input(let)) {
+    cout << let;
+} else {
+    cout << "errore";
+  }
   return 0;
 }
